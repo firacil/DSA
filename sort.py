328ab126534a9433f024fd4bddf4f4691c8f7cb6
Firaol Mk <firaolmekbib511@gmail.com>
2025-03-02 23:42:56 +0300

    sort method in python

---
+++ sort.py
@@ -0,0 +1,10 @@
+# Initializing a list
+a = [5, 2, 9, 1, 5, 6]
+
+# Sorting the list in ascending order
+a.sort()
+print("Sorted list (ascending):", a)
+
+# Sorting the list in descending order
+a.sort(reverse=True)
+print("Sorted list (descending):", a)
